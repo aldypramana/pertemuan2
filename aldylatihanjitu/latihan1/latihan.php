d4387d57ebba68fa570e9368d38cf3d58a7f5f89
aldypramana <127809021+aldypramana@users.noreply.github.com>
2023-03-18 11:17:54 +0700

    I KETUT ALDY PRAMANA PUTRA (21101144)
    latihan ke 2

---
+++ aldylatihanjitu/latihan1/latihan.php
@@ -0,0 +1,8 @@
+<?php
+    echo "Belajar script PHP";
+    $dta ="jagad";
+    $angka = 5;
+    echo '<br>nama '. $dta. " peringkat ke- ".$angka;
+    echo '<br>nama  $dta' . "anak ke-". $angka;
+    echo "<br>nama: $dta anak ke-$angka";
+     
---
+++ aldylatihanjitu/latihan1/latihan4.php
@@ -0,0 +1,17 @@
+<?php
+$nVar = "lima";
+switch($nVar){
+    case "satu":
+        echo "nvar bernilai satu";
+        break;
+    case "dua":
+        echo "nvar bernilai dua";
+        break;
+     case "tiga":
+        echo "nvar bernilai tiga";
+         break;
+    default :
+         echo "nvar bernilai acak";
+         break;
+    
+}
